20d56aa95b4824db53452065c155be094e2db14e
WarrenOliver <warren@oliver.us>
2023-12-30 15:06:48 -0500

    updated instructions

--- credentials-example.py
+++ credentials-example.py
@@ -1,6 +1,10 @@
 import os
 
-# ARTI API KEY
+# rename file to credentials.py
+# you will also need Google Oauth2 credentials. These you should rename to credentials.json -- A token.json and token.pickle file will be generated once your key is present and verified.
+
+# OpenAI API Key
 os.environ['OPENAI_API_KEY'] = '<your openai API key>'
 
+# Login Password
 os.environ['LOGIN_PASSWORD'] = '<login password of your choosing>'
